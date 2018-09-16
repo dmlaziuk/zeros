cc777914917729286e23dd091483f4c5476d3f42
dmlaziuk <dm.laziuk@gmail.com>
2018-09-17 02:59:31 +0300

    Implement function getZerosCount(number) version 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
-}
+  let mod5 = 0;
+  for (let num = 2; num <= number; num += 1) {
+    for (let nn = num; nn % 5 === 0; nn /= 5) {
+      mod5 += 1;
+    }
+  }
+  return mod5;
+};
